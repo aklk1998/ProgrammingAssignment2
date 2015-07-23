97090adc54087319d18ab1def8c71c246e20221e
aklk1998 <aklk1998@gmail.com>
2015-07-23 18:03:24 -0400

    my first draft

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,32 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+##  1.set the value of the vector
+  
+  m <- NULL
+  setmatrix <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+##  2.get the value of the vector
+  
+  getmatrix <- function() x
+  
+##  3.set the value of the inversed matrix
+  
+  setimatrix <- function(imatrix) m <<- imatrix
+  
+##  4.get the value of the inverse matrix
+  
+  getimatrix <- function() m
+  
+## Assign the functions to a list
+  
+  list(setmatrix = setmatrix, getmatrix = getmatrix,
+       setimatrix = setimatrix,
+       getimatrix = getimatrix)
+  
 }
 
 
@@ -12,4 +38,30 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+## check whehter the matrix has been inversed and saved
+  
+  m <- x$getimatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+## Retrieve the matrix
+  
+  data <- x$getmatrix()
+  
+## inverse the matrix
+  
+  m <- solve(data, ...)
+  
+## Save the inversed matrix
+  
+  x$setimatrix(m)
+  
+## Return inversed matrix
+  
+  m
+  
+  
 }
